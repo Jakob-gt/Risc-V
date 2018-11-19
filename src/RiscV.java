7c7de9928b05edf40c6613d8732197627da9a3d4
Jakob G. Thomsen <jakob-gt@hotmail.com>
2018-11-19 20:36:06 +0100

    Fixed problem with EOF-Exception when reading in loop.bin file.

--- src/RiscV.java
+++ src/RiscV.java
@@ -25,39 +25,78 @@ public class RiscV {
             int rs2 = (instr >> 20) & 0x01f;
             int imm = 0;
 
+            boolean count = true;
+
             switch (opcode) {
+
 
                 case 0x37:    //LUI
                     imm = instr & 0xfffff000;
                     reg[rd] = imm;
                     break;
 
+                case 0x17:    //AUIPC
+                    imm = instr & 0xfffff000;
+                    reg[rd] = imm + pc;
+                    break;
+
+                case 0x6f:    //JAL
+                    imm = (((instr >> 22) & 0x3ff) << 1) + (((instr >> 21) & 0x1) << 11)
+                            + (((instr >> 12) & 0xff) << 12) + (((instr >> 31) & 0x1) << 20) ;
+                    if ((instr >>> 31) == 1) {
+                        imm |= 0xffe00000;
+                    }
+                    reg[rd] = pc+1;
+                    pc = imm;
+                    count = false;
+                    break;
+
+                case 0x67:    //JALR
+                    imm = (instr >>> 20);
+                    reg[rd] = reg[rs1]+1;
+                    pc = imm;
+                    count = false;
+                    break;
+
+
                 case 0x63:   // B-type
-                    imm = (((instr >>> 8) & 0x0f) << 1) + (((instr >>> 25) & 0x3f) << 5)
-                            + (((instr >>> 7) & 0x1) << 11) + (((instr >>> 31) & 0x1) << 12) ;
+                    imm = (((instr >> 8) & 0x0f) << 1) + (((instr >> 25) & 0x3f) << 5)
+                            + (((instr >> 7) & 0x1) << 11) + (((instr >> 31) & 0x1) << 12) ;
                     if ((instr >>> 31) == 1) {
                         imm |= 0xffffe000;
                     }
-
+                    imm /= 4;
                     switch (funct3) {
 
                         case 0x00:             // BEQ
-                            if (reg[rs1] == reg[rs2]) {pc += imm;}
+                            if (reg[rs1] == reg[rs2]) {
+                                pc += imm;
+                                count = false;
+                            }
                             break;
                         case 0x01:             // BNE
-                            if (reg[rs1] != reg[rs2]) {pc += imm;}
+                            if (reg[rs1] != reg[rs2]) {
+                                pc += imm;
+                                count = false;
+                            }
                             break;
                         case 0x04:             // BLT
-                            if (reg[rs1] < reg[rs2]) {pc += imm;}
+                            if (reg[rs1] < reg[rs2]) {
+                                pc += imm;
+                                count = false;
+                            }
                             break;
                         case 0x05:             // BGE
-                            if (reg[rs1] >= reg[rs2]) {pc += imm;}
+                            if (reg[rs1] >= reg[rs2]) {
+                                pc += imm;
+                                count = false;
+                            }
                             break;
                     }
                     break;
 
                 case 0x13:
-                    imm = (instr >>> 20);
+                    imm = (instr >> 20);
                     switch (funct3) {
 
                         case 0x00:             // ADDI
@@ -78,8 +117,16 @@ public class RiscV {
                         case 0x01:             // SLLI
                             reg[rd] = reg[rs1] << imm;
                             break;
-                        case 0x05:             // SRLI
-                            reg[rd] = reg[rs1] << imm;
+                        case 0x05:
+                            switch (funct7) {
+
+                                case 0x00:         // SRLI
+                                    reg[rd] = reg[rs1] >>> imm;
+                                    break;
+                                case 0x20:         // SRAI
+                                    reg[rd] = reg[rs1] >> imm;
+                                    break;
+                            }
                             break;
                     }
                     break;
@@ -129,8 +176,9 @@ public class RiscV {
                     break;
             }
 
-
-            ++pc; // We count in 4 byte words
+            if (count) {
+                pc += 1; // We count in 4 byte words
+            }
 
             if (pc >= progr.size()) {
                 break;
@@ -139,9 +187,10 @@ public class RiscV {
             for (int i = 0; i < reg.length; ++i) {
                 System.out.print(reg[i] + " ");
             }
-            System.out.println();
+            System.out.println(pc);
+
         }
-        writeOutputFile();
+        //writeOutputFile(); //Turn on to write output file
         System.out.println("Program exit");
     }
 
@@ -149,9 +198,15 @@ public class RiscV {
 	public static void readBinFile(String filedestination) throws IOException{
 		DataInputStream binfile = new DataInputStream(new FileInputStream(filedestination));
 		while(binfile.available() > 0) {
-			int instruction = binfile.readInt();
-			instruction = Integer.reverseBytes(instruction);
-			progr.add(instruction);
+            try
+            {
+                int instruction = binfile.readInt();
+                instruction = Integer.reverseBytes(instruction);
+                progr.add(instruction);
+            }
+            catch(EOFException e) {
+                //just catches the error.. only in loop.bin
+            }
 		}
 		binfile.close();
 	}
